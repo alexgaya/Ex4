f5008cedd258f5f0c5b6643728fd1505f89c2c84
Alejandro Gaya Gonzalez <alejandrogaya1994@gmail.com>
2021-03-22 16:01:09 +0100

    Add Helper, Add data files

---
+++ src/Helpers/ConstantsHelper.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Helpers;
+
+/**
+ *
+ * @author m
+ */
+public class ConstantsHelper {
+    public static final String DATA_FOLDER = "data";
+    public static final String MASTER = "Articles.dat";
+    public static final String NMASTER = "NewArticles.dat";
+    public static final String ANOM = "Anomalies.dat";
+    public static final String UPDATE = "Updates.dat";
+}
